3e44c10ccb5295c055535639ee24d87639263e1d
Yancap <yangabriel3001f@gmail.com>
2023-03-30 23:40:17 -0300

    configuração do move das janelas

--- src/Components/Windows/EventMove.js
+++ src/Components/Windows/EventMove.js
@@ -6,7 +6,7 @@ export class EventMove{
     handleDown(event){
         const element = event.currentTarget
         element.addEventListener('mousemove', this.handleMove)
-        element.addEventListener('mouseout', this.handleUp)
+        //element.addEventListener('mouseout', this.handleUp)
     }
     handleUp(event){
         this.click.x = 0
@@ -15,22 +15,18 @@ export class EventMove{
         this.start.y = null
         const element = event.currentTarget
         element.removeEventListener('mousemove', this.handleMove)
-        element.removeEventListener('mouseout', this.handleUp)
+        //element.removeEventListener('mouseout', this.handleUp)
     }
     handleMove(event){
         const element = event.currentTarget
         if (!this.start.x && !this.start.y) {
-            console.log('a');
             this.start.x = event.offsetX
             this.start.y = event.offsetY
         }
-        this.click.x = element.getBoundingClientRect().left + (event.screenX - element.getBoundingClientRect().left - (this.start.x - 1))
-        this.click.y = element.getBoundingClientRect().top + (event.screenY - element.getBoundingClientRect().top - (this.start.y - 1))
-        console.log(element.getBoundingClientRect().left + (event.screenX - element.getBoundingClientRect().left - (this.start.x - 1)));
+        this.click.x = element.getBoundingClientRect().left + (event.clientX - element.getBoundingClientRect().left - (this.start.x - 1))
+        this.click.y = element.getBoundingClientRect().top + (event.clientY - element.getBoundingClientRect().top - (this.start.y - 1))
         element.style.left =  this.click.x + "px"
         element.style.top = this.click.y + "px"
-        
-        
     }
     bindEvents(){
         this.handleDown = this.handleDown.bind(this)
