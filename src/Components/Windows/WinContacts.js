e575f98e173d19e75b67d100c0d82f05e7189f00
Yancap <yangabriel3001f@gmail.com>
2023-04-04 20:54:27 -0300

    estilização dos contatos

--- src/Components/Windows/WinContacts.js
+++ src/Components/Windows/WinContacts.js
@@ -1,14 +1,31 @@
 import React from 'react'
 import { Window } from './Window'
-import styles from './WinProject.module.scss'
+import styles from './WinContacts.module.scss'
 import icon from '../../Assets/desktop/contact.png'
+import { Button } from '../Common/Button'
 
 export const WinContacts = () => {
   return (
-    
       <Window icon={icon} path='Contatos' target='contact'>
-        <div >
-          Contact
+        <div className={styles.contact}>
+          <form action="">
+             <div className={styles.label}>
+              <label htmlFor='name' >Nome</label>
+              <input id='name' autocomplete='off'/>
+             </div>
+             <div className={styles.label}>
+                <label htmlFor='email' >Email</label>
+                <input id='email' autoComplete='off'/>
+             </div>
+             <div className={styles.label}>
+                <label htmlFor='message' >Mensagem</label>
+                <textarea id='message' autoComplete='off'/>
+              </div>
+              <Button text='Enviar'/>
+            </form>
+            <div>
+              
+            </div>
         </div>
       </Window>
     
