48b2254aa97f0f866ea6a694f0fa189fc3df5680
Yancap <yangabriel3001f@gmail.com>
2023-04-04 21:15:15 -0300

    estilização do contact

--- src/Components/Windows/WinContacts.js
+++ src/Components/Windows/WinContacts.js
@@ -3,6 +3,12 @@ import { Window } from './Window'
 import styles from './WinContacts.module.scss'
 import icon from '../../Assets/desktop/contact.png'
 import { Button } from '../Common/Button'
+import { ReactComponent as Git } from '../../Assets/contact/git.svg'
+import { ReactComponent as Linkedin } from '../../Assets/contact/linkedin.svg'
+import { ReactComponent as Instagram } from '../../Assets/contact/instagram.svg'
+import { ReactComponent as Email } from '../../Assets/contact/email.svg'
+import { ReactComponent as Local } from '../../Assets/contact/local.svg'
+import { ReactComponent as Phone } from '../../Assets/contact/phone.svg'
 
 export const WinContacts = () => {
   return (
@@ -23,8 +29,32 @@ export const WinContacts = () => {
               </div>
               <Button text='Enviar'/>
             </form>
-            <div>
-              
+            <div className={styles.network}>
+              <div className={styles.link}>
+                <Linkedin />
+                <a href="https://www.linkedin.com/in/yangabriel">https://www.linkedin.com/in/yangabriel/</a>
+              </div>
+              <div className={styles.link}>
+                <Git />
+                <a href="https://github.com/Yancap">https://github.com/Yancap</a>
+              </div>
+              <div className={styles.link}>
+                <Instagram />
+                <a href="">none</a>
+              </div>
+              <div className={styles.link}>
+                <Local />
+                <a href="">Rio de Janeiro, RJ, Brasil</a>
+              </div>
+              <div className={styles.link}>
+                <Email />
+                <a href="">yangabriel3001f@gmail.com</a>
+              </div>
+              <div className={styles.link}>
+                <Phone />
+                <a href="">(21) 99164-2554</a>
+              </div>
+
             </div>
         </div>
       </Window>
