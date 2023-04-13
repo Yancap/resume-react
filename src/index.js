1bba95bdb1098405b9d0c5bb98c271eeaa41c47c
Yancap <yangabriel3001f@gmail.com>
2023-04-13 16:20:00 -0300

    teste de fallback

--- src/index.js
+++ src/index.js
@@ -4,9 +4,10 @@ import App from './App';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`);
 root.render(
     <App />
-  
 );
 
 
